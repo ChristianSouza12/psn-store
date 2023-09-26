810dc8312e844932a9545d8cfcff2c1a9b6c8264
ChristianSouza12 <christianlmsouza@outlook.com>
2023-09-26 10:26:51 -0300

    creat project

---
+++ src/app/components/card/card-pricing/card-pricing.component.ts
@@ -0,0 +1,20 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card-pricing',
+  templateUrl: './card-pricing.component.html',
+  styleUrls: ['./card-pricing.component.css']
+})
+export class CardPricingComponent implements OnInit {
+
+  @Input()
+  gameType:string = ""
+  @Input()
+  gamePrice:string = ""
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,22 @@
+import { Component, Input, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrls: ['./card.component.css']
+})
+export class CardComponent implements OnInit {
+  @Input()
+  gameCover:string=""
+  @Input()
+  gameLabel:string = ""
+  @Input()
+  gameType:string = ""
+  @Input()
+  gamePrice:string = ""
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
